dcb7897a0f044661d6d76ca26809ac7c79f9b6ca
Gregory Domorski <gdomorski@paypal.com>
2019-02-01 11:41:30 -0800

    init

---
+++ README.md
@@ -0,0 +1,63 @@
+# Passport-Yahoo!-Contacts
+
+[Passport](http://passportjs.org/) strategies for authenticating with [Yahoo!](http://www.yahoo.com/)
+using the OAuth 2.0 API.
+
+This module lets you authenticate using Yahoo! in your Node.js applications.
+By plugging into Passport, Yahoo! authentication can be easily and
+unobtrusively integrated into any application or framework that supports
+[Connect](http://www.senchalabs.org/connect/)-style middleware, including
+[Express](http://expressjs.com/).
+
+## Installation
+
+    $ npm install passport-yahoo!-oauth
+
+#### Configure Strategy
+
+The Yahoo authentication strategy authenticates users using a Yahoo account
+and OAuth tokens.  The strategy requires a `verify` callback, which accepts
+these credentials and calls `done` providing a user, as well as `options`
+specifying a consumer key, consumer secret, and callback URL.
+
+    passport.use(new YahooStrategy({
+        clientID: YAHOO_CONSUMER_KEY,
+        clientSecret: YAHOO_CONSUMER_SECRET,
+        callbackURL: "http://127.0.0.1:3000/auth/yahoo/callback"
+      },
+      function(token, tokenSecret, profile, done) {
+        User.findOrCreate({ yahooId: profile.id }, function (err, user) {
+          return done(err, user);
+        });
+      }
+    ));
+
+#### Authenticate Requests
+
+Use `passport.authenticate()`, specifying the `'yahoo'` strategy, to
+authenticate requests.
+
+For example, as route middleware in an [Express](http://expressjs.com/)
+application:
+
+    app.get('/auth/yahoo',
+      passport.authenticate('yahoo'));
+
+    app.get('/auth/yahoo/callback',
+      passport.authenticate('yahoo', { failureRedirect: '/login' }),
+      function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+      });
+
+## Examples
+
+For a complete, working example, refer to the [login example](https://github.com/gdomorski/passport-yahoo!-contacts/tree/master/examples/login).
+
+## Credits
+  - [Greg Domorski](http:/github.com/gdomorski)
+  - [Jared Hanson](http://github.com/jaredhanson)
+
+## License
+
+[The MIT License](http://opensource.org/licenses/MIT)
---
+++ examples/login/app.js
@@ -0,0 +1,125 @@
+var express = require('express')
+  , passport = require('passport')
+  , util = require('util')
+  , YahooStrategy = require('../../lib/passport-yahoo-contacts/index.js').Strategy
+  , logger = require('morgan')
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
+  , session = require('express-session');
+
+
+
+var YAHOO_CONSUMER_KEY = "dj0yJmk9Q1loYWVzYkxrTnNwJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTdl"
+var YAHOO_CONSUMER_SECRET = "f98d29abdcb48fae7f47d0887c0ae8d23e0ac653";
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Yahoo profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the YahooStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a token, tokenSecret, and Yahoo profile), and
+//   invoke a callback with a user object.
+passport.use(new YahooStrategy({
+  clientID: YAHOO_CONSUMER_KEY,
+  clientSecret: YAHOO_CONSUMER_SECRET,
+  callbackURL: "http://www.greg.com/auth/yahoo/callback",
+  passReqToCallback: true,
+  },
+  function(request, token, tokenSecret, profile, done) {
+    request.session.yahoo = { queryCode: request.query.code }
+    return done(null, profile, 'HELLO WORLD')
+  }
+));
+
+
+
+
+var app = express();
+
+// configure Express
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(cookieParser());
+app.use(session({
+    secret: 'blah'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// GET /auth/yahoo
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Yahoo authentication will involve redirecting
+//   the user to yahoo.com.  After authorization, Yahoo will redirect the user
+//   back to this application at /auth/yahoo/callback
+app.get('/auth/yahoo',
+  passport.authenticate('yahoo'),
+  function(req, res){
+
+  });
+
+// GET /auth/yahoo/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/yahoo/callback',
+  passport.authenticate('yahoo',
+  function(req, res){
+    // The request will be redirected to Microsoft Graph for authentication, so
+    // this function will not be called.
+  }
+  ))
+
+
+
+
+
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(80);
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
